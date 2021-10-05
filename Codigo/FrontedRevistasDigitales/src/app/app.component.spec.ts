a8aec5734996acd2d48e267510ea45d39861b24d
JoelEnriquez <jsem1331@gmail.com>
2021-10-04 21:53:24 -0600

    Generacion SQL

--- Codigo/FrontedRevistasDigitales/src/app/app.component.spec.ts
+++ Codigo/FrontedRevistasDigitales/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Iteracion1'`, () => {
+  it(`should have as title 'FrontedRevistasDigitales'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Iteracion1');
+    expect(app.title).toEqual('FrontedRevistasDigitales');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Iteracion1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FrontedRevistasDigitales app is running!');
   });
 });
